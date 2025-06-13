f5d41dba53728ea61afb803834848b52e459e3fd
annibuliful <lagmanmc@gmail.com>
2025-06-13 11:24:03 +0700

    feat: check token on dashboard path

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,9 +1,29 @@
 import { Fragment } from 'react';
+import { ACCESS_TOKEN_KEY, ACCOUNT_ID_KEY } from '../../constant/api';
+import { headers } from 'next/headers';
+import { parseCookiesFromHeader } from '../../utils/cookies';
+import { verifyAuthoizedAccount } from '../../api/authorization';
+import { redirect } from 'next/navigation';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieHeader = (await headers()).get('cookie');
+  const parseCookies = parseCookiesFromHeader(cookieHeader);
+  const accountId = parseCookies[ACCOUNT_ID_KEY];
+  const accessToken = parseCookies[ACCESS_TOKEN_KEY];
+
+  if (!accessToken) {
+    redirect('/');
+  }
+
+  const isVerified = await verifyAuthoizedAccount({ accessToken, accountId });
+
+  if (!isVerified) {
+    redirect('/');
+  }
+
   return <Fragment>{children}</Fragment>;
 }
